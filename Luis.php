8dd3b7c99e7b1a91394812bef0b5960d057c8643
Luis Perdomo <Luis25867611@gmail.com>
2018-11-11 23:53:11 -0400

    Luis.php
    Tarea Programacion

---
+++ Luis.php
@@ -0,0 +1,18 @@
+<?php
+    class Hola{
+    public $Nombre;
+    public $Apellido;
+    public $Cedula;
+    function Escribir(){
+        echo "El Nombre Es: ". $this->Nombre."<br>"."<br>";
+        echo "El Apellido Es: ".$this->Apellido."<br>"."<br>";
+        echo "La Cedula Es: ".$this->Cedula."<br>"."<br>";
+    }
+    }
+    $Hola1 = New Hola();
+
+    $Hola1 ->Nombre="Luis";
+    $Hola1 ->Apellido="Perdomo";
+    $Hola1 ->Cedula="25.867.611";
+    $Hola1 ->Escribir();
+?>
